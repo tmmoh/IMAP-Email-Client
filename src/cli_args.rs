49f170cf0ce67d30621c58f4b3248707ae8e0cc6
Taher Mohamed <tmmoh@student.unimelb.edu>
2024-05-04 10:40:02 +0000

    implemented command line parsing

---
+++ src/cli_args.rs
@@ -0,0 +1,177 @@
+use std::{num::ParseIntError, str::FromStr};
+
+
+#[derive(Debug)]
+pub enum ArgParseError {
+    Missing,
+    Duplicate,
+    Invalid,
+}
+
+impl From<ParseIntError> for ArgParseError {
+    fn from(_: ParseIntError) -> Self {
+        Self::Invalid
+    }
+}
+
+#[derive(Debug)]
+enum Command {
+    Retrieve,
+    Parse,
+    Mime,
+    List,
+}
+
+impl FromStr for Command {
+    type Err = ArgParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "retrieve" => Ok(Self::Retrieve),
+            "parse" => Ok(Self::Parse),
+            "mime" => Ok(Self::Mime),
+            "list" => Ok(Self::List),
+            _ => Err(ArgParseError::Invalid),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct InputArgs {
+    username: String,
+    password: String,
+    folder: Option<String>,
+    message_num: Option<u32>,
+    tls: bool,
+    command: Command,
+    server_name: String,
+}
+
+impl TryFrom<Vec<String>> for InputArgs {
+    type Error = ArgParseError;
+
+    fn try_from(mut args: Vec<String>) -> Result<Self, Self::Error> {
+        args.remove(0);
+        let mut args_iter = args.into_iter();
+        let mut args_builder = InputArgsBuilder::default();
+
+        let mut next_arg = || args_iter.next().ok_or(ArgParseError::Missing);
+
+        args_builder = loop {
+            let arg = next_arg()?;
+            args_builder = match arg.as_str() {
+                "-u" => args_builder.username(next_arg()?)?,
+                "-p" => args_builder.password(next_arg()?)?,
+                "-f" => args_builder.folder(next_arg()?)?,
+                "-n" => args_builder.message_num(next_arg()?.trim().parse()?)?,
+                "-t" => args_builder.tls(true)?,
+                _ => break args_builder.command(arg)?.server_name(next_arg()?)?,
+            };
+        };
+
+        args_builder.build()
+    }
+}
+
+#[derive(Default)]
+struct InputArgsBuilder {
+    username: Option<String>,
+    password: Option<String>,
+    folder: Option<String>,
+    message_num: Option<u32>,
+    tls: Option<bool>,
+    command: Option<Command>,
+    server_name: Option<String>,
+}
+
+impl InputArgsBuilder {
+    pub fn username(mut self, username: impl Into<String>) -> Result<Self, ArgParseError> {
+        if self.username.is_some() {
+            Err(ArgParseError::Duplicate)
+        } else {
+            let _ = self.username.insert(username.into());
+            Ok(self)
+        }
+    }
+
+    pub fn password(mut self, password: impl Into<String>) -> Result<Self, ArgParseError> {
+        if self.password.is_some() {
+            Err(ArgParseError::Duplicate)
+        } else {
+            let _ = self.password.insert(password.into());
+            Ok(self)
+        }
+    }
+
+    pub fn folder(mut self, folder: impl Into<String>) -> Result<Self, ArgParseError> {
+        if self.folder.is_some() {
+            Err(ArgParseError::Duplicate)
+        } else {
+            let _ = self.folder.insert(folder.into());
+            Ok(self)
+        }
+    }
+
+    pub fn message_num(mut self, n: u32) -> Result<Self, ArgParseError> {
+        if self.message_num.is_some() {
+            Err(ArgParseError::Duplicate)
+        } else {
+            let _ = self.message_num.insert(n);
+            Ok(self)
+        }
+    }
+
+    pub fn tls(mut self, tls: bool) -> Result<Self, ArgParseError> {
+        if self.tls.is_some() {
+            Err(ArgParseError::Duplicate)
+        } else {
+            let _ = self.tls.insert(tls);
+            Ok(self)
+        }
+    }
+
+    pub fn command(mut self, command_str: impl Into<String>) -> Result<Self, ArgParseError> {
+        let command = Command::from_str(&command_str.into())?;
+
+        if self.command.is_some() {
+            Err(ArgParseError::Duplicate)
+        } else {
+            let _ = self.command.insert(command);
+            Ok(self)
+        }
+    }
+
+    pub fn server_name(mut self, server_name: impl Into<String>) -> Result<Self, ArgParseError> {
+        if self.server_name.is_some() {
+            Err(ArgParseError::Duplicate)
+        } else {
+            let _ = self.server_name.insert(server_name.into());
+            Ok(self)
+        }
+    }
+
+    fn build(self) -> Result<InputArgs, ArgParseError> {
+        let Some(username) = self.username else {
+            return Err(ArgParseError::Missing);
+        };
+        let Some(password) = self.password else {
+            return Err(ArgParseError::Missing);
+        };
+        let Some(command) = self.command else {
+            return Err(ArgParseError::Missing);
+        };
+        let Some(server_name) = self.server_name else {
+            return Err(ArgParseError::Missing);
+        };
+
+        Ok(InputArgs {
+            username,
+            password,
+            folder: self.folder.clone(),
+            message_num: self.message_num,
+            tls: self.tls.unwrap_or_default(),
+            command,
+            server_name,
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
+use crate::cli_args::InputArgs;
+
+pub mod cli_args;
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    dbg!(args.clone());
+    dbg!(InputArgs::try_from(args).expect("invalid args"));
 }
